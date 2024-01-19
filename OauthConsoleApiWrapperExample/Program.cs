f45c29ab6c290d02b85c05ddefc41f4158c4db40
Conrad <conrad@hh-dev.com>
2024-01-19 11:33:51 -0600

    Added Oauth example

---
+++ OauthConsoleApiWrapperExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using HHDev.DataManagement.ApiClientWrapper;
+using HHDev.DataManagement.Api.Models;
+using HHDev.DataManagement.Client.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HHDev.Core.Helpers;
+
+namespace OauthConsoleApiWrapperExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Running OAuth console HH DM API Wrapper example.");
+
+            // The same AuthenticationSettings instance must be shared between OAuthAuthenticationManager and the AuthenticationManager given to the ApiClient instance.
+            var sharedAuthSettings = new SimpleAuthenticationSettings
+            {
+                AuthenticationMode = eAuthenticationMode.OAuth
+            };
+            OAuthAuthenticationManager.AuthenticationSettings = sharedAuthSettings;
+            var apiClient = new ApiClient(new AuthenticationManager(sharedAuthSettings));
+
+            var getAccountsResult = await apiClient.GetAllAccounts(new ApiGetOptions());
+
+            if (getAccountsResult.Success == false)
+            {
+                Console.WriteLine("Failed to get accounts");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine($"Retrieved accounts:");
+            Console.WriteLine(string.Join("\n", getAccountsResult.ReturnValue.Select(a => $"{a.Name}: {a.Id}")));
+            Console.ReadKey();
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository contains two different solutions with some examples of how the HH DM .NET API wrapper can be used.
 
-In both examples an [API key](https://help.hh-dm.com/extensibility/api/#authentication) is needed to authenticate against the API.
+In all examples except for the OAuth specific one, an [API key](https://help.hh-dm.com/extensibility/api/#authentication) is needed to authenticate against the API.
 
 ### SimpleConsoleApiWrapperExample
 
@@ -15,3 +15,9 @@ To get started, the `API_KEY`, `accountId` and `runSheetId` variables need to be
 This is a more complete example built as a WPF application.  The application allows the user to select an account, championship, event and car.  The setups for this event and car are then loaded.  The user can select a setup, and then edit it or delete it.  New setups can also be created.  The attached files for the selected setup can also be downloaded, deleted or new files can be added.
 
 To get started, the `API_KEY` variable need to be populated with values.
+
+### OAuthConsoleApiWrapperExample
+
+This is a very simple example that uses OAuth to retrieve all the accounts available to the user that logs in.
+
+This example does not need to be populated with any values and can be run as-is.
